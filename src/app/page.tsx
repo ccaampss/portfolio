52c39caa9e87299e57784e1deaa0d7d884172799
ccaampss <annacampsromero@gmail.com>
2023-11-06 23:28:55 +0100

    Add project links to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import { projects } from "@/data";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Anna Camps (En proceso)
-      <div className="flex gap-8">
-        <Link
-          href="https://www.chat.annacamps.es/"
-          className="text-md  bg-sky-400 text-sky-50 px-2 py-1 rounded-lg"
-        >
-          Chat App
-        </Link>
+      <div className="flex gap-8 flex-wrap justify-center">
+        {projects.map((project) => (
+          <Link
+            href={project.url}
+            key={project.name}
+            className={`text-md font-medium text-white ${project.color} px-2 py-1 rounded-lg`}
+          >
+            {project.name}
+          </Link>
+        ))}
       </div>
     </main>
   );
---
+++ src/data.ts
@@ -0,0 +1,13 @@
+interface Project {
+  name: string;
+  url: string;
+  color: string;
+}
+
+export const projects: Project[] = [
+  {
+    name: "Chat App",
+    url: "https://www.chat.annacamps.es/",
+    color: "bg-sky-400",
+  },
+];
